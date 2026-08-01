04730f38a1f1c6bac99d8479a7d1864d62fd8f1f
hussnainmughal123 <97694727+hussnainmughal123@users.noreply.github.com>
2026-08-02 04:41:42 +0500

    Add cart routes for managing shopping cart

---
+++ backend/routes/cartRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getCart,
+  addToCart,
+  updateCartItem,
+  removeFromCart,
+  clearCart,
+} = require('../controllers/cartController');
+const { protect } = require('../middleware/auth');
+
+router.use(protect);
+
+router.route('/').get(getCart).post(addToCart).delete(clearCart);
+router.route('/:productId').put(updateCartItem).delete(removeFromCart);
+
+module.exports = router;
